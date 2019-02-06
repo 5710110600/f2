5304ad315c54138c7278de243f21f2151a4196cf
5710110600 <38688020+5710110600@users.noreply.github.com>
2019-02-06 15:15:12 +0700

    homework04

---
+++ ex04/Game.java
@@ -0,0 +1,50 @@
+public class Game{
+    private int p1;
+    private int p2;
+    private int maxSlot;
+    private int turn;
+    private Dice dice1;
+    private Dice dice2;
+
+    public Game(int max_slot){
+        p1 = 0;
+        p2 = 0;
+        maxSlot = max_slot;
+        turn = 1;
+        dice1 = new Dice();
+        dice2 = new Dice();
+    }
+    
+    public void walk(int slot){
+        if(turn == 1){
+            p1 += slot;
+            System.out.println("Player1=" + p1);
+        }else if(turn == 2){
+            p2 += slot;
+            System.out.println("Player2=" + p2);
+        }
+        if(p1 >= maxSlot){
+            System.out.println("Player1 win");
+        }else if(p2 >= maxSlot){
+            System.out.println("Player2 win");
+        }
+    }
+
+    public boolean isEnded(){
+        return p1 >= maxSlot || p2 >= maxSlot; 
+    }
+
+    public void play(){
+        dice1.roll();
+        System.out.println("dice1 =" + dice1.getValue()  );
+        dice2.roll();
+        System.out.println("dice2 =" + dice2.getValue()  );
+        walk(dice1.getValue() + dice2.getValue());
+        if(dice1.getValue() != dice2.getValue())
+        changTurn();
+    }
+
+    public void changTurn(){
+        turn = turn == 1 ? 2 : 1;
+    }
+}
